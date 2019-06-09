beea11fb24cb22fbc13bd3c9250e348770e95155
josepmg <josemello@id.uff.br>
2019-06-08 21:44:12 -0300

    Criacao de filtro de monitoramente do tempo de execucao e WebService(SOAP e REST).

---
+++ src/java/br/uff/carrinho/controler/ProdudoService.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uff.carrinho.controler;
+
+import br.uff.carrinho.model.Produto;
+import br.uff.carrinho.model.ProdutoDAO;
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author JP
+ */
+public class ProdudoService extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Recupera todos produtos do Banco de dados
+        List<Produto> listaProdutos = (ArrayList<Produto>)(new ProdutoDAO()).listaTodos();
+        
+        String valor = request.getHeader("accept");
+        if(valor.contains("xml")){
+            // XML
+            XStream xStream = new XStream();
+            xStream.alias("Produto", Produto.class);
+            String xml = xStream.toXML(listaProdutos);
+
+            response.setContentType("application/xml");
+            response.getWriter().print(xml);
+        } else if (valor.contains("json")){
+            // JSON
+            Gson gson = new Gson();
+            String json = gson.toJson(listaProdutos); 
+
+            response.setContentType("application/json");
+            response.getWriter().print(json);
+        } 
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
